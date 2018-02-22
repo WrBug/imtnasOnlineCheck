807c78e4e75a58e433a5a39c0223c987dab5d856
WrBug <i@wrbug.com>
2018-02-23 00:36:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ mkdir -p checkOnline
 cd checkOnline
 echo '正在获取最新脚本'
 wget https://raw.githubusercontent.com/WrBug/imtnasOnlineCheck/master/check.sh -O check.sh
+txt=$(cat /etc/crontab)
+if [[ ${txt} == *'checkOnline'* ]]; then
+echo '定时任务已存在，跳过'
+else
+echo '正在配置定时任务'
 echo '*/1 * * * *  root cd /home/checkOnline && sh check.sh' >> /etc/crontab
+fi
 service cron restart
 echo '脚本配置成功'
