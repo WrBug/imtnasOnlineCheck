e93b1c6ff6a49b772f429db06ed0e439f941bcdc
WrBug <i@wrbug.com>
2018-02-23 21:50:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,15 @@ echo '项目地址： https://github.com/WrBug/imtnasOnlineCheck'
 echo 'Android版钱包,支持IMT：https://www.token.soy'
 cd /usr
 mkdir -p checkOnline
+rm -rf /home/checkOnline
 cd checkOnline
 echo '正在获取最新脚本'
 wget https://raw.githubusercontent.com/WrBug/imtnasOnlineCheck/master/check.sh -O check.sh
 get https://raw.githubusercontent.com/WrBug/imtnasOnlineCheck/master/checkCpu.py -O checkCpu.py
 txt=$(cat /etc/crontab)
+if [[ ${txt} == *'/home/checkOnline'* ]]; then
+echo '检测到旧版本数据，请编辑 /etc/crontab文件。  删除 */1 * * * *  root cd /home/checkOnline && sh check.sh 这一行'
+fi
 if [[ ${txt} == *'/usr/checkOnline'* ]]; then
 echo '定时任务已存在，跳过'
 else
