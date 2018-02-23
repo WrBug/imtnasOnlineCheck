481c3042f6fb5f49f8d40b0c5fadc75d945f98af
WrBug <i@wrbug.com>
2018-02-23 21:40:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 echo '星际魔盒离线检测脚本'
 echo '项目地址： https://github.com/WrBug/imtnasOnlineCheck'
 echo 'Android版钱包,支持IMT：https://www.token.soy'
-cd /home
+cd /usr
 mkdir -p checkOnline
 cd checkOnline
 echo '正在获取最新脚本'
 wget https://raw.githubusercontent.com/WrBug/imtnasOnlineCheck/master/check.sh -O check.sh
 get https://raw.githubusercontent.com/WrBug/imtnasOnlineCheck/master/checkCpu.py -O checkCpu.py
 txt=$(cat /etc/crontab)
-if [[ ${txt} == *'checkOnline'* ]]; then
+if [[ ${txt} == *'/usr/checkOnline'* ]]; then
 echo '定时任务已存在，跳过'
 else
 echo '正在配置定时任务'
-echo '*/1 * * * *  root cd /home/checkOnline && sh check.sh' >> /etc/crontab
+echo '*/1 * * * *  root cd /usr/checkOnline && sh check.sh' >> /etc/crontab
 fi
 service cron restart
 echo '脚本配置成功,服务已启动'
