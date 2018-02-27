39b1270161efa7b86196de9c0197c55e2740938c
WrBug <i@wrbug.com>
2018-02-27 23:12:11 +0800

    Update checkCpu.py
    添加sleepingTask检测

--- checkCpu.py
+++ checkCpu.py
@@ -1,7 +1,17 @@
 import os,re
-if      __name__=="__main__":
+if __name__=="__main__":
         output = os.popen('top -n 1 |grep Cpu | cut -d "," -f 4 | cut -d " " -f 2')
         avaCpu=output.read()
         avaCpu=re.search(r'\d+',avaCpu).group()
         avaCpu = int(str(avaCpu))
-        print avaCpu<20
+        if avaCpu<20 :
+                print 'Cpu'
+        else :
+                output = os.popen('top -n 1 |grep Task | cut -d "," -f 3 | cut -d " " -f 2')
+                sleepTask=output.read()
+                sleepTask=re.search(r'\d+',sleepTask).group()
+                sleepTask = int(str(sleepTask))
+                if sleepTask<500 :
+                        print 'Normal'
+                else:
+                        print 'Tasks'
